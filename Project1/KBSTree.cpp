28d2f40e5e8f5361b9869542559995bace08ef44
Loner0822 <Loner0822@users.noreply.github.com>
2018-10-10 17:54:55 +0800

    2018年10月10日17:54:48

--- Project1/KBSTree.cpp
+++ Project1/KBSTree.cpp
@@ -12,27 +12,39 @@ WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
     try
     {
-         Application->Initialize();
-         Application->CreateForm(__classid(TDMod), &DMod);
-         Application->CreateForm(__classid(TForm1), &Form1);
-         Application->CreateForm(__classid(TExtraForm), &ExtraForm);
-         Application->CreateForm(__classid(TForm2), &Form2);
-         Application->Run();
+        Application->Initialize();
+        // 判断重复启动
+        Application->Title = "集中监测标准知识库";
+        HANDLE hnd = CreateMutex(NULL, TRUE, "集中监测标准知识库");
+        if (NULL == hnd) {
+            return 0;
+        }
+        if (GetLastError() == ERROR_ALREADY_EXISTS) {
+            ::MessageBox(NULL, "程序已经运行", Application->Title.c_str(), MB_OK|MB_SYSTEMMODAL);
+            ReleaseMutex(hnd);
+            CloseHandle(hnd);
+            return 0;
+        }
+        Application->CreateForm(__classid(TDMod), &DMod);
+        Application->CreateForm(__classid(TForm1), &Form1);
+        Application->CreateForm(__classid(TExtraForm), &ExtraForm);
+        Application->CreateForm(__classid(TForm2), &Form2);
+        Application->Run();
     }
     catch (Exception &exception)
     {
-         Application->ShowException(&exception);
+        Application->ShowException(&exception);
     }
     catch (...)
     {
-         try
-         {
-             throw Exception("");
-         }
-         catch (Exception &exception)
-         {
-             Application->ShowException(&exception);
-         }
+        try
+        {
+            throw Exception("");
+        }
+        catch (Exception &exception)
+        {
+            Application->ShowException(&exception);
+        }
     }
     return 0;
 }
